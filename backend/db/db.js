0ec1881379f7280ad85f1ba13020d740521dbbd5
Garvit Asthana <garvit.asthana@gmail.com>
2024-12-14 16:39:26 +0530

    2nd

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDb() {
     mongoose.connect(process.env.DB_CONNECT ).then(() => {
-        console.log("connected to db");
+        console.log("connected to database");
     }).catch(err => console.log(err));
 }
 
