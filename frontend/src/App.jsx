8050769152f5c90161e450e2033cd29ac216a00f
Garvit Asthana <garvit.asthana@gmail.com>
2025-01-29 13:05:24 +0530

    integrate userLogin and userSignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import {Route , Routes} from 'react-router-dom';
-import Home from './pages/home';
+import Start from './pages/Start';
 import UserLogin from './pages/UserLogin';
 import UserSignUp from './pages/usersignup';
 import CaptainLogin from './pages/CaptainLogin';
 import CaptainSignUp from './pages/CaptainSignup';
+import Home from './pages/home';
 
 const App = () => {
   return (
     <div >
      <Routes>
-      <Route path='/' element={<Home/>} />
+      <Route path='/' element={<Start/>} />
       <Route path='/login' element={<UserLogin/>} />
       <Route path='/signup' element={<UserSignUp/>} />
       <Route path='/captain-login' element={<CaptainLogin/>} />
       <Route path='/captain-signup' element={<CaptainSignUp/>} />
+      <Route path='/home' element={<Home />} />
       
 
      </Routes>
