ef0ce55f9e643d5f42e3bee87b68a928060876c9
Garvit Asthana <garvit.asthana@gmail.com>
2025-02-03 14:41:25 +0530

    home page create

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const LocationSearchPanel = () => {
+    return <div>
+        {/* this is just sample data */}
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium"> 24B, near kapoor cafe, School, Bhopal</h4>
+        </div>
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium"> 24B, near kapoor cafe, School, Bhopal</h4>
+        </div>
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium"> 24B, near kapoor cafe, School, Bhopal</h4>
+        </div>
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium"> 24B, near kapoor cafe, School, Bhopal</h4>
+        </div>
+        <div className="flex gap-4 items-center my-4 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-10 rounded-full"><i className="ri-map-pin-fill"></i></h2>
+            <h4 className="font-medium"> 24B, near kapoor cafe, School, Bhopal</h4>
+        </div>
+        
+    </div>
+}
+
+export default LocationSearchPanel;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,124 @@
-import React from 'react';
+import React  from 'react';
+import { useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+import { useRef, useState } from 'react';
+import 'remixicon/fonts/remixicon.css';
+import LocationSearchPanel from '../components/LocationSearchPanel';
 
 const Home = () => {
+    const [pickup, setPickup] = useState('');
+    const [destination, setDestination] = useState('');
+    const [panelOpen , setPanelOpen] = useState(false);
+    const panelRef = useRef(null);
+    const panelCloseRef = useRef(null);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        
+    }
+
+    useGSAP(function(){
+        if(panelOpen) {
+            gsap.to(panelRef.current, {
+                height: '70%',
+                padding:24
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 1
+            })
+        }
+        else {
+            gsap.to(panelRef.current, {
+                height: '0%',
+                padding:0
+                
+
+            })
+            gsap.to(panelCloseRef.current, {
+                opacity: 0
+            })
+        }
+    },[panelOpen]);
+
+
     return (
-        <div>
-            Home
+        <div className='h-screen relative overflow-hidden'>
+            <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <div className='h-screen w-screen'>
+                {/* image for temporary use */}
+                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+            </div>
+            <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
+              <div className='h-[30%] p-6 bg-white relative'>
+                <h5 ref={panelCloseRef} onClick={()=> {
+                    setPanelOpen(false)
+                }} className='absolute opacity-0 top-6 right-6 text-2xl'>
+                    <i className="ri-arrow-down-wide-line"></i>
+                </h5>
+              <h4 className='text-2xl font-semibold'>Find a trip</h4>
+                <form onSubmit={(e) => {
+                    submitHandler(e)
+                }}>
+                    <div className="line absolute h-16 w-1 top-[43%] left-10 bg-gray-700 rounded-full"></div>
+                    <input
+                    onClick={() => {
+                        setPanelOpen(true)
+                    }}
+                    value={pickup}
+                    onChange={(e)=> {
+                        setPickup(e.target.value)
+                    }} 
+                    className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5' 
+                    type="text" 
+                    placeholder='Add a pick up location' 
+                    />
+                    <input
+                    onClick={() => {
+                        setPanelOpen(true)
+                    }}
+                    value={destination}
+                    onChange={(e)=> {
+                        setDestination(e.target.value)
+                    }} 
+                    className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' 
+                    type="text" 
+                    placeholder='Enter your destination'/>
+                </form>
+              </div>
+              <div ref={panelRef} className=' bg-white h-0'>
+                    <LocationSearchPanel />
+              </div>
+            </div>
+            <div className='fixed w-full z-10 bottom-0 bg-white px-3 py-6'>
+                <h3 className='text-2xl font-semibold mb-5'>Choose a vehicle</h3>
+                    <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-cetner justify-between'>
+                        <img className='h-10' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ7Kt54z31PkbdlqmqnyWnaCjvcLYRG-T_8Q&s' />
+                        <div className='-ml-2 w-1/2'>
+                            <h4 className='font-medium text-base'>UberGo <span><i className='ri-user-3-fill'>4</i></span></h4>
+                            <h5 className='font-medium text-sm'>2 mins away</h5>
+                            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+                        </div>
+                        <h2 className='text-lg font-semibold'>Rs 193.20</h2>
+                    </div>
+                    <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-cetner justify-between'>
+                        <img className='h-10' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png' />
+                        <div className='-ml-2 w-1/2'>
+                            <h4 className='font-medium text-base'>Moto <span><i className='ri-user-3-fill'>1</i></span></h4>
+                            <h5 className='font-medium text-sm'>3 mins away</h5>
+                            <p className='font-normal text-xs text-gray-600'>Affordable, motorcycle rides</p>
+                        </div>
+                        <h2 className='text-lg font-semibold'>Rs 65.40</h2>
+                    </div>
+                    <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-cetner justify-between'>
+                        <img className='h-10' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png' />
+                        <div className='-ml-2 w-1/2'>
+                            <h4 className='font-medium text-base'>UberAuto<span><i className='ri-user-3-fill'>3</i></span></h4>
+                            <h5 className='font-medium text-sm'>2 mins away</h5>
+                            <p className='font-normal text-xs text-gray-600'>Affordable auto rides</p>
+                        </div>
+                        <h2 className='text-lg font-semibold'>Rs 118.68</h2>
+                    </div>
+            </div>
         </div>
     )
 }
